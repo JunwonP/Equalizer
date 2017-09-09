f5e3b1e8bbf216378abd725442b6a752747b5049
JunwonP <merlin0876@gmail.com>
2017-09-09 20:00:05 +0900

    Add multiple arrangement

--- Equalizer/app/src/main/java/com/equalizer/hyosori/equalizer/presenter/SetterPresenter.java
+++ Equalizer/app/src/main/java/com/equalizer/hyosori/equalizer/presenter/SetterPresenter.java
@@ -42,6 +42,28 @@ public class SetterPresenter implements Presenter {
         this.target = new BandSet(targetName);
 
         this.view.setSeekBars(this.target.getBandSet());
+        int[] a = new int[3];
+        int[] A_freqList = {60, 50, 40, 30};
+        int[] B_freqList = {40, 50, 60, 30};
+        float[] multiply = new float[4];
+        int y = 0;
+        for(int i=1;i<10;i++) {
+            if(A_freqList[y] > A_freqList[i]) {
+                y = i;
+            }
+        }
+        for(int i=1;i<10;i++) {
+            if(B_freqList[y] > B_freqList[i]) {
+                y = i;
+            }
+        }
+        //가장작은값은 A_freqList[y] (y)번째
+        for(int i=0;i<4;i++) {
+            if(i==y) {
+                multiply[i] = 1;
+            }
+            multiply[i] = A_freqList[i]/A_freqList[y];
+        }
     }
 
     public void onSeekBarChanged(int frequency, int amplitude) {
